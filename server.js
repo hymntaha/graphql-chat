c69086a98e7054e7ab8f9afeaef45abd78e0e326
hymntaha <tahauygun@gmail.com>
2020-11-04 22:01:48 -0800

    db connection

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const {ApolloServer} = require('apollo-server');
+const {sequelize} = require('./models');
+const envConfigs =  require('./config/config');
+const env = process.env.NODE_ENV || 'development';
+const config = envConfigs[env];
+const Sequelize = require('sequelize');
+
 
 // A map of functions which returns data for the schema
 const resolvers = require('./graphql/resolvers');
@@ -9,6 +15,9 @@ const server = new ApolloServer({
   resolvers
 })
 
+let Sequel = new Sequelize(config.url, config);
+
 server.listen().then(({url}) => {
   console.log(` Server ready at ${url}`)
+  Sequel.authenticate().then(() => console.log('Database is connected!!!')).catch(err=> console.error(err));
 })
