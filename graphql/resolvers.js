cb2c2db83483d5677b4f42f2132f355cf85227d1
hymntaha <tahauygun@gmail.com>
2020-11-04 23:28:14 -0800

    register mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -12,4 +12,27 @@ module.exports = {
       }
     },
   },
+  Mutation: {
+    register: async (_, args, context, info) => {
+      const { username, email, password, confirmPassword } = args;
+
+      try {
+        // TODO: Validate input data
+
+        // TODO: Check if username / email exists
+
+        // TODO: Create user
+        const user = await User.create({
+          username,
+          email,
+          password,
+        });
+
+        // TODO: Return user
+        return user;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  },
 };
