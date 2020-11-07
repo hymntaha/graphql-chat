78927d667cac7e6e9d031518438d1ab1b22f5404
hymntaha <tahauygun@gmail.com>
2020-11-06 22:58:38 -0800

    client work

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import "./App.scss";
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Register</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
