033343bd57d6e6c91fc0e9cee8ad65151a9b9359
hymntaha <tahauygun@gmail.com>
2020-11-07 22:06:31 -0800

    adding routes for login & home

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import ApolloProvider from "./ApolloProvider";
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Register from "./Components/Register";
+import Login from "./Components/Login";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <ApolloProvider>
-      <Container className="pt-5">
-        <Register />
-      </Container>
+      <BrowserRouter>
+        <Container className="pt-5">
+          <Switch>
+            <Route path="/register" component={Register} />
+            <Route path="/" component={Home} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
---
+++ client/src/Components/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Login(props) {
+  return (
+    <div>
+      <h1>Login Page</h1>
+    </div>
+  );
+}
