3e69bb7e42771f9951d99c956e5af575f389523a
hymntaha <tahauygun@gmail.com>
2020-11-09 22:50:55 -0800

    fetching all users and messages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
@@ -30,17 +30,38 @@ export default function Home({ history }) {
     console.log(data);
   }
 
+  let usersMarkup;
+  if (!data || loading) {
+    usersMarkup = <p>Loading...</p>;
+  } else if (data.getUsers.length === 0) {
+    usersMarkup = <p>No users have joined yet</p>;
+  } else if (data.getUsers.length > 0) {
+    usersMarkup = data.getUsers.map((user) => (
+      <div key={user.username}>
+        <p>{user.username}</p>
+      </div>
+    ));
+  }
+
   return (
-    <Row className="bg-white justify-content-around">
-      <Link to="/login">
-        <Button variant="link">Login</Button>
-      </Link>
-      <Link to="/register">
-        <Button variant="link">Register</Button>
-      </Link>
-      <Button variant="link" onClick={logout}>
-        Login
-      </Button>
-    </Row>
+    <Fragment>
+      <Row className="bg-white justify-content-around">
+        <Link to="/login">
+          <Button variant="link">Login</Button>
+        </Link>
+        <Link to="/register">
+          <Button variant="link">Register</Button>
+        </Link>
+        <Button variant="link" onClick={logout}>
+          Login
+        </Button>
+      </Row>
+      <Row>
+        <Col xs={4}>{usersMarkup}</Col>
+        <Col xs={8}>
+          <p>Messages</p>
+        </Col>
+      </Row>
+    </Fragment>
   );
 }
