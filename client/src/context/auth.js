aba48185f73d59355ad752b979ee6c231f813969
hymntaha <tahauygun@gmail.com>
2020-11-09 22:20:29 -0800

    route gards

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,7 +1,23 @@
 import React, { createContext, useReducer, useContext } from "react";
-
+import jwtDecode from "jwt-decode";
 const AuthStateContext = createContext();
 const AuthDispatchContext = createContext();
+
+let user;
+const token = localStorage.getItem("token");
+if (token) {
+  const decodedToken = jwtDecode(token);
+  const expiresAt = new Date(decodedToken.exp * 1000);
+
+  if (new Date() > expiresAt) {
+    localStorage.removeItem("token");
+  } else {
+    user = decodedToken;
+  }
+  console.log(expiresAt);
+} else {
+  console.log("No token found!");
+}
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -13,6 +29,7 @@ const authReducer = (state, action) => {
         user: action.payload,
       };
     case "LOGOUT":
+      localStorage.removeItem("token");
       return {
         ...state,
         user: null,
@@ -23,7 +40,7 @@ const authReducer = (state, action) => {
 };
 
 export const AuthProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, { user: null });
+  const [state, dispatch] = useReducer(authReducer, { user });
   return (
     <AuthDispatchContext.Provider value={dispatch}>
       <AuthStateContext value={state}>{children}</AuthStateContext>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export default function Home() {
+import { useAuthDispatch } from "../context/auth";
+
+export default function Home({ history }) {
+  const dispatch = useAuthDispatch();
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    history.push("/login");
+  };
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <Row className="bg-white justify-content-around">
+      <Link to="/login">
+        <Button variant="link">Login</Button>
+      </Link>
+      <Link to="/register">
+        <Button variant="link">Register</Button>
+      </Link>
+      <Button variant="link" onClick={logout}>
+        Login
+      </Button>
+    </Row>
   );
 }
