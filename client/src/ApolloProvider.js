a04ce8f3c50ef12ba198badba7bf89b38b474ba1
hymntaha <tahauygun@gmail.com>
2020-11-09 22:46:59 -0800

    fix

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -24,7 +24,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
