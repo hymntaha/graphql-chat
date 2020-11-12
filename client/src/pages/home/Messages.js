3aa9119d961cecefb78a87cf48ba24832adbeb74
hymntaha <tahauygun@gmail.com>
2020-11-11 23:20:23 -0800

    refactoring and styling 3

--- client/src/pages/home/Messages.js
+++ client/src/pages/home/Messages.js
@@ -15,28 +15,44 @@ const GET_MESSAGES = gql`
 `;
 export default function Messages() {
   const { users } = useMessageState();
-  const selectedUser = users?.find((u) => u.selected === true)?.username;
+  const dispatch = useMessageDispatch();
 
+  const selectedUser = users?.find((u) => u.selected === true);
+  const messages = selectedUser?.messages;
   const [
     getMessages,
     { loading: messagesLoading, data: messagesData },
   ] = useLazyQuery(GET_MESSAGES);
 
   useEffect(() => {
-    if (selectedUser) {
-      getMessages({ variables: { from: selectedUser } });
+    if (selectedUser && !selectedUser.messages) {
+      getMessages({ variables: { from: selectedUser.username } });
     }
   }, [selectedUser]);
 
-  return (
-    <Col xs={8}>
-      {messagesData && messagesData.getMessages.length > 0 ? (
-        messagesData.getMessages.map((message) => (
-          <p key={message.uuid}>{message.content}</p>
-        ))
-      ) : (
-        <p>Messages</p>
-      )}
-    </Col>
-  );
+  useEffect(() => {
+    if (messagesData) {
+      dispatch({
+        type: "SET_USER_MESSAGES",
+        payload: {
+          username: selectedUser.username,
+          messages: messagesData.getMessages,
+        },
+      });
+    }
+  }, [messagesData]);
+
+  let selectedChatMarkup;
+  if (!messages && !messagesLoading) {
+    selectedChatMarkup = <p>Select a friend</p>;
+  } else if (messagesLoading) {
+    selectedChatMarkup = <p>Loading...</p>;
+  } else if (messages.length > 0) {
+    selectedChatMarkup = map((message) => (
+      <p key={message.uuid}>{message.content}</p>
+    ));
+  } else if (messages.length === 0) {
+    selectedChatMarkup = <p>You are now connected! Send your first message</p>;
+  }
+  return <Col xs={8}>{selectedChatMarkup}</Col>;
 }
