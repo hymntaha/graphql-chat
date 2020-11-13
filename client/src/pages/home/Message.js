3eff6d7823f8edd7c9d7c84741a76691f5272b4e
hymntaha <tahauygun@gmail.com>
2020-11-12 23:48:32 -0800

    message styling and decoupling

--- client/src/pages/home/Message.js
+++ client/src/pages/home/Message.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useAuthState } from "../../context/auth";
+import classNames from "classNames";
 
 export default function Message({ message }) {
+  const { user } = useAuthState();
+
+  const sent = message.from === user.username;
+  const received = !sent;
+
   return (
-    <div className="py-2 px3 rounded-pill bg-primary">
-      <p key={message.uuid}>{message.content}</p>
+    <div className="d-flex my-3">
+      <div
+        className={classNames("py-2 px3 rounded-pill bg-primary", {
+          "bg-primary": sent,
+          "bg-secondary": received,
+        })}
+      >
+        <p className={classNames({ "text-white": sent })} key={message.uuid}>
+          {message.content}
+        </p>
+      </div>
     </div>
   );
 }
